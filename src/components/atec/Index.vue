5f200c04bfcd472a672eb86ac8da4b74851f96a5
alansarluv <alan.sarluf@gmail.com>
2019-12-14 23:53:44 +0700

    setup basic route installment

---
+++ src/components/atec/Index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Atec - Index</h1>
+</template>
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import Dashboard from './components/Dashboard.vue';
+import AtecIndex from './components/atec/Index.vue';
+
+export const routes = [
+  { path: '/', component: Dashboard },
+  { path: '/atec', component: AtecIndex },
+
+];
