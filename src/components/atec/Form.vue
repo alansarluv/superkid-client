6bbac8de6a821f36f98e7c24ec9de0c1da7af1d9
alansarluv <alan.sarluf@gmail.com>
2020-01-05 23:34:27 +0700

    setup atec form

--- src/components/atec/Form.vue
+++ src/components/atec/Form.vue
@@ -1,26 +1,23 @@
 <template>
-  <div class="container-fluid mt-100">
-    <div class="scrollable-box">
-      <form action="/atec/form-report" method="POST">
-        <input type="hidden" name="_csrf" value="<%= csrfToken %>">
-        <input type="hidden" name="kidName" value="<%= kids[0].name %>">
-        <div class="row">
-          <div class="col col-12 mb-4">
-            <div class="row mb-4">
-              <div class="col-md-12 col-lg-6 col-xl-4">
-                <div class="card">
-                  <div class="card-body">
-                    <p>Nama : Dummy name</p>
-                    <p>Jenis kelamin : Dummy Age</p>
-                    <p>Birth day : birthdayString</p>
-                    <input type="hidden" class="available-report-date" value="<%= listReportMonthYear %>">
-                  </div>
-                </div>
+  <div class="container-fluid mt-125">
+    <form action="/atec/form-report" method="POST">
+      <input type="hidden" name="_csrf" value="<%= csrfToken %>">
+      <input type="hidden" name="kidName" value="<%= kids[0].name %>">
+
+      <div class="row justify-content-center">
+        <div class="col-lg-4 col-md-6 col-sm-12">
+          <div class="scrollable-box">
+            <div class="card mb-4">
+              <div class="card-body">
+                <p>Nama : Dummy name</p>
+                <p>Jenis kelamin : Dummy Age</p>
+                <p>Birth day : birthdayString</p>
+                <input type="hidden" class="available-report-date" value="<%= listReportMonthYear %>">
               </div>
             </div>
-            <h3>Piih Bulan dan Tahun laporan ATEC</h3>
+            <h3 class="mb-3">Piih Bulan dan Tahun laporan ATEC</h3>
             <div class="row">
-              <div class="col-md-12 col-lg-3 col-xl-2 mb-3">
+              <div class="col-md-12 col-lg-6 mb-3">
                 <select name="atecMonth" class="form-control jc-auto-select jc-check-available-monthyear" data-type-auto="month">
                   <option value="00">Januari</option>
                   <option value="01">Februari</option>
@@ -36,7 +33,7 @@
                   <option value="11">Desember</option>
                 </select>
               </div>
-              <div class="col-md-12 col-lg-3 col-xl-2 mb-3">
+              <div class="col-md-12 col-lg-6 mb-3">
                 <select name="atecYear" class="form-control jc-auto-select jc-check-available-monthyear" data-type-auto="year">
                   <option value="2021">2021</option>
                   <option value="2020">2020</option>
@@ -49,13 +46,12 @@
             </div>
           </div>
         </div>
-        <hr class="separator">
-        <div class="row atec-form-style">
-          <div class="col-sm-12 col-md-10 col-lg-8">
-            <p>question here</p>
+        <div class="col-lg-4 col-md-6 col-sm-12">
+          <div class="scrollable-box">
+            <p> Question here</p>
           </div>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   </div>
 </template>
