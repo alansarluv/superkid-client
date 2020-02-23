4e388df6816683e5f06c7038ae8e5b8415dc855d
alansarluv <alan.sarluf@gmail.com>
2020-02-23 19:57:13 +0700

    Add spinner mixins

--- src/mixins/general.js
+++ src/mixins/general.js
@@ -1,8 +1,15 @@
 export const generalMixin = {
-  // data() {
-  //   return {
-  //   }
-  // },
+  data() {
+    return {
+      spinner: `
+        <div class="d-flex justify-content-center">
+          <div class="spinner-border text-secondary" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>      
+      `
+    }
+  },
   methods: {
     getAge (birthDate) {
       const today = new Date();
