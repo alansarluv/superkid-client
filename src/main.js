8d904c06c5a03ae50bb4c0b13fe580597475329f
alansarluv <alan.sarluf@gmail.com>
2020-04-12 14:40:46 +0700

    update default baseURL

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { routes } from './routes'
 import axios from 'axios'
 import store from './store'
 
-axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/http://superkid.id:3000'
+axios.defaults.baseURL = 'https://api.superkid.id'
 // axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODA3NDU1ODYsImV4cCI6MTU4MDkxODM4NiwiaXNzIjoiaHR0cHM6Ly9zY290Y2guaW8ifQ.DXbDvUWe-jhX3aB3gRiAXrDLlYb9izsICGhvfm-xoIE'
 // axios.defaults.headers.get['Accepts'] = 'application/json'
 
