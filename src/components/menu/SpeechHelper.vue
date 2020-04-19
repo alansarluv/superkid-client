c3ca3021d18bd1e615021d89b8179c56485eb23e
alansarluv <alan.sarluf@gmail.com>
2020-04-19 16:00:03 +0700

    Add speech recognition and image

--- src/components/menu/SpeechHelper.vue
+++ src/components/menu/SpeechHelper.vue
@@ -2,56 +2,60 @@
   <div class="container-fluid">
     <div class="row justify-content-center">
       <div class="col-sm-12 my-3 text-center">
-        <h3 class="text-center mb-3">Speech text reader</h3>
+        <h3 class="text-center mb-3">Speech Helper</h3>
         <div 
-          class="btn btn-primary" 
+          class="btn btn-primary c-pointer" 
           data-toggle="collapse" 
           data-target="#collapseBox" 
           aria-expanded="false" 
-          @click="getVoices()"
           aria-controls="collapseBox">
-            Click untuk tampilkan box
+            Klik untuk menulis sendiri
           </div>
-        <div id="collapseBox" class="card text-box collapse">
-          <div id="close" class="close-box">&times;</div>
-          <div class="text-left">
-            <h3>Pilih bahasa</h3>
-            <select 
-              id="voices" 
-              v-model="speech.selectedLang"
-              @change="setVoice()">
-              <option 
-                v-for="(voice, idx) in voices" 
-                :key="idx"
-                :value="voice.name">
-                {{voice.name}} - {{voice.lang}}
-              </option>
-            </select>
-          </div>
-          <div class="form-group">
-            <textarea 
-              id="text" 
-              v-model="speech.textarea"
-              class="form-control mt-3" 
-              placeholder="Tulis kalimat yang ingin disuarakan"
-              rows="3">
-            </textarea>
-          </div>
-          <button 
-            class="btn btn-block btn-warning" 
-            @click="readText()"
-            id="read">
-            Bunyikan
-          </button>
-        </div>
       </div>
       <div class="col-sm-12">
+        <div class="row">
+          <div class="col-sm-12">
+            <div id="collapseBox" class="text-box collapse">
+              <div id="close" class="close-box d-none">&times;</div>
+              <div class="text-left">
+                <h3>Pilih bahasa</h3>
+                <select 
+                  id="voices" 
+                  v-model="speech.selectedLang"
+                  @change="setVoice()">
+                  <template v-for="(voice, idx) in voices">
+                    <option 
+                      :key="idx"
+                      :value="voice.name">
+                      {{voice.name}} - {{voice.lang}}
+                    </option>
+                  </template>
+                </select>
+              </div>
+              <div class="form-group">
+                <textarea 
+                  id="text" 
+                  v-model="speech.textarea"
+                  class="form-control mt-3" 
+                  placeholder="Tulis kalimat yang ingin disuarakan"
+                  rows="3">
+                </textarea>
+              </div>
+              <button 
+                class="btn btn-block btn-warning" 
+                @click="speakText(speech.textarea)"
+                id="read">
+                Bunyikan
+              </button>
+            </div>
+          </div>
+        </div>
         <div class="row mt-4 justify-content-center">
           <div v-for="(list, idx) in dataLIst" :key="idx" class="col-sm-3 mb-3">
             <div @click="speakText(list.text)" class="card c-pointer">
               <div class="card-body">
                 <h5 class="card-title">{{list.text}}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <img :src="list.src" :alt="list.text">
               </div>
             </div>            
           </div>
@@ -66,72 +70,55 @@
       return {
         dataLIst: [
           {
-            image: './img/haus.jpg',
+            src: '../../assets/speech-activity/minum.jpg',
             text: "Aku haus, aku mau minum"
           },
           {
-            image: './img/makan.jpg',
+            src: '../../assets/speech-activity/makan.jpg',
             text: "Aku lapar, aku mau makan"
           },
           {
-            image: './img/capek.jpg',
+            src: '../../assets/speech-activity/senang.jpg',
             text: "Aku capek"
           },
           {
-            image: './img/hurt.jpg',
-            text: "Aku sakisakitt"
+            src: '../../assets/speech-activity/senang.jpg',
+            text: "Aku sakit"
           },
           {
-            image: './img/senang.jpg',
+            src: '../../assets/speech-activity/senang.jpg',
             text: "Aku senang"
           },
           {
-            image: './img/marah.jpg',
+            src: '../../assets/speech-activity/marah.jpg',
             text: "Aku marah"
           },
           {
-            image: './img/sedih.jpg',
+            src: '../../assets/speech-activity/sedih.jpg',
             text: "Aku sedih"
           },
           {
-            image: './img/takut.jpg',
+            src: '../../assets/speech-activity/takut.jpg',
             text: "Aku takut"
           },
           {
-            image: './img/jalanjalan.jpg',
+            src: '../../assets/speech-activity/berjalan.jpg',
             text: 'Aku mau jalan jalan'
           },
           {
-            image: './img/pulang.jpg',
+            src: '../../assets/speech-activity/pulang.jpg',
             text: 'Aku mau pulang'
           },
           {
-            image: './img/sekolah.jpg',
+            src: '../../assets/speech-activity/sekolah.jpg',
             text: 'Aku mau ke sekolah'
           },
           {
-            image: './img/bermain.jpg',
+            src: '../../assets/speech-activity/bermain.jpg',
             text: 'Aku mau bermain'
           }
         ],
-        voices: [
-          {
-            val: 'Google Bahasa Indonesia',
-            name: 'Bahasa Indonesia'
-          },
-          {
-            val: 'Google US English',
-            name: 'US English'
-          },
-          {
-            val: 'Google UK English Female',
-            name: 'UK English Female'
-          },
-          {
-            val: 'Google UK English Male',
-            name: 'UK English Male'
-          }
-        ],
+        voices: [],
         message: new SpeechSynthesisUtterance(),
         speech: {
           textarea: '',
@@ -140,24 +127,44 @@
       }
     },
     methods: {
+      // get option language to speech
       getVoices() {
-        this.voices = speechSynthesis.getVoices();
+        const allVoices = speechSynthesis.getVoices();
+
+        if (allVoices.length) {
+          // sort option language for only 4 languages
+          const _this = this;
+          allVoices.map(x => {
+            if (x.lang === 'en-US' || x.lang === 'id-ID' || x.lang === 'en-GB') {
+              _this.voices.push(x);
+            }
+          })
+        }
         this.message.voice = this.voices.find(voice => voice.name === this.speech.selectedLang);
       },
-      readText() {
-        this.message.text = this.speech.textarea;
-        speechSynthesis.speak(this.message);
+      // init default language as bahasa
+      initDefaultVoices() {
+        const runInterval = setInterval(() => {
+          this.getVoices();
+          if (this.voices.length) {
+            // stop spinner loading option languages
+            clearInterval(runInterval);
+          }
+        }, 3000);
       },
+      // call this function to speak some text param
       speakText(text){
         this.message.text = text;
         speechSynthesis.speak(this.message);
       },
+      // on change language options, run this function
       setVoice() {
         this.message.voice = this.voices.find(voice => voice.name === this.speech.selectedLang);
-      }
+      },
     },
     mounted() {
-      this.getVoices();
+      // run spinner loading while waiting on option languages
+      this.initDefaultVoices();
     }
   }
 </script>
@@ -167,8 +174,9 @@
     max-width: 680px;
     margin: 20px auto 0;
     padding: 12px 16px;
-    height: 270px;
     color: white;
+    border-radius: 4px;
+    position: relative;
     .close-box {
       position: absolute;
       right: 10px;
