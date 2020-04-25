1809135a7afda1a3f6fb0fd02db5c06d680ea7af
alansarluv <alan.sarluf@gmail.com>
2020-04-25 23:49:53 +0700

    Update speech rcognition using vue

--- src/components/menu/ColorPlay.vue
+++ src/components/menu/ColorPlay.vue
@@ -1,67 +1,89 @@
 <template>
-  <div class="container-fluid bgColorCustom">
+  <div class="container-fluid bgColorCustom" :style="{ backgroundColor: selectedColor }">
     <div class="row justify-content-center">
       <div class="col-sm-12 my-3 text-center">
         <h3 class="text-center mb-3">Sebut Warna</h3>
-        <div class="btn btn-primary c-pointer btnTrigger">Click disini untuk mulai</div>
+        <div @click="startGame" class="btn btn-primary c-pointer btnTrigger">Click disini untuk mulai</div>
         <div class="mt-5">
           <p class="hints"></p>
         </div>
         <div class="mt-6">
-          <p class="output"></p>
+          <p v-if="transcript.length">Kamu menyebutkan warna {{transcript}}</p>
+        </div>
+        <div class="mt-6">
+          <p>{{hints}}</p>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+  const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;  // eslint-disable-line no-undef
 
-  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;  // eslint-disable-line no-undef
+  export default {
+    data() {
+      return {
+        listWarna:
+        [
+          'merah', 'hijau', 'biru', 'kuning', 'orange', 'ungu', 'pink',
+          'kelabu', 'coklat', 'abu-abu', 'putih', 'hitam', 'jingga'
+        ],
+        colorList:
+        [
+          'red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink',
+          'grey', 'chocolate', 'grey', 'white', 'black', 'orange'
+        ],
 
-  var listWarna = ['merah', 'hijau', 'biru', 'kuning', 'orange', 'ungu', 'pink', 'kelabu', 'abu-abu', 'putih', 'hitam', 'jingga'];
-  var colorList = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'grey', 'grey', 'white', 'black', 'orange'];
+        recognition: new SpeechRecognition(),
+        hints: '',
+        transcript: '',
+        confidence: 0,
+        selectedColor: 'white'
+      }
+    },
+    methods: {
+      configSetup(){
+        this.recognition.continuous = false;
+        this.recognition.lang = 'id-ID';
+        this.recognition.interimResults = false;
+        this.recognition.maxAlternatives = 1;
+      },
+      startGame(){
+        this.recognition.start();
+        this.hints = "Sebutkan warna apa saja";
+        this.transcript = "";
 
-  var recognition = new SpeechRecognition();
-  recognition.continuous = false;
-  recognition.lang = 'id-ID';
-  recognition.interimResults = false;
-  recognition.maxAlternatives = 1;
+        const _this = this;
+        this.recognition.onresult = function(event) {
+          _this.result(event);
+        }
+        this.recognition.onspeechend = function() {
+          _this.hints = "";
+          _this.recognition.stop();
+        }
 
-  var diagnostic = document.querySelector('.output');
-  
-  document.addEventListener("DOMContentLoaded", function(){
-    const btnTrigger = document.querySelector('.btnTrigger');
-    btnTrigger.addEventListener('click', function(){
-      recognition.start();
-      console.log('Ready to receive a color command.'); // eslint-disable-line no-console
-    })
-  });
+        this.recognition.onerror = function(event) {
+          this.hints = 'Error occurred in recognition: ' + event.error;
+        }
+      },
+      result(event){
+        const rec = event.results[0][0].transcript;
+        this.confidence = event.results[0][0].confidence;
 
-  recognition.onresult = function(event) {
-    var selectedColor = document.querySelector('.output');
-    var color = event.results[0][0].transcript;
-    color = color.toLocaleLowerCase();
-    
-    selectedColor.innerText = "warna "+ color;
-    var indexArr = 0;
-    if (listWarna.includes(color)){
-      indexArr = listWarna.indexOf(color);
+        this.transcript = rec.toLocaleLowerCase();
+        console.log("you said : " + this.transcript);           // eslint-disable-line no-console
+        console.log("convidence level : " + this.confidence);   // eslint-disable-line no-console
+
+        if (this.listWarna.includes(this.transcript)){
+          this.selectedColor = this.colorList[this.listWarna.indexOf(this.transcript)];
+        } else {
+          this.hints = `Kami belum punya warna ${this.transcript}`
+        }
+      }
+    },
+    mounted(){
+      this.configSetup();
     }
-    const bg = document.querySelector('.bgColorCustom');
-    bg.style.backgroundColor = colorList[indexArr];
-    console.log('Confidence: ' + event.results[0][0].confidence); // eslint-disable-line no-console
-  }
-
-  recognition.onspeechend = function() {
-    recognition.stop();
-  }
-
-  recognition.onnomatch = function(event) { // eslint-disable-line no-unused-vars
-    diagnostic.textContent = "I didn't recognise that color.";
-  }
-
-  recognition.onerror = function(event) {
-    diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
   }
 </script>
 <style lang="scss">
