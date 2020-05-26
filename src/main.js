886d89be008ef637a34527e55104f4fd5ffbe7d7
alansarluv <alan.sarluf@gmail.com>
2020-05-26 23:17:53 +0700

    registering service worker for PWA needs

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ import { routes } from './routes'
 import axios from 'axios'
 import store from './store'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./sw.js')
+    .then(function(){
+      console.log("yihaa service worker registered"); // eslint-disable-line no-console
+    })
+}
+
 axios.defaults.baseURL = 'https://api.superkid.id'
 // axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODA3NDU1ODYsImV4cCI6MTU4MDkxODM4NiwiaXNzIjoiaHR0cHM6Ly9zY290Y2guaW8ifQ.DXbDvUWe-jhX3aB3gRiAXrDLlYb9izsICGhvfm-xoIE'
 // axios.defaults.headers.get['Accepts'] = 'application/json'
